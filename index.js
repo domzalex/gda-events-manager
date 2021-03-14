2a5e3b98eb97f105db339009b818698a9a4cbe4e
domzalex <alexdomzalski@gmail.com>
2021-03-13 19:13:53 -0500

    case error on user model

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
-const User = require("./models/User");
+const User = require("./models/user");
 const Events = require("./models/events");
 const app = express();
 
